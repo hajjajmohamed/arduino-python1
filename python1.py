091a62a0d97a0e4cdf9a3a81f3133e2ab088f74c
the inventor hero <habasama895@gmail.com>
2025-10-31 15:00:23 +0100

    Refactor and clean up hand LED control code

--- python1.py
+++ python1.py
@@ -2,18 +2,13 @@ import cv2
 import mediapipe as mp
 from pyfirmata import Arduino, util
 import time
-
-# Set Arduino Port
-port = "COM8"  # Change this to your port (e.g., COM3, /dev/ttyUSB0)
+port = "COM8" 
 board = Arduino(port)
 led = board.get_pin("d:13:o")
 
-# MediaPipe setup
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
-
-# Start camera
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -28,10 +23,9 @@ while True:
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 lmList.append((id, cx, cy))
-            
-            # Check finger position
+        
             if lmList:
-                if lmList[8][2] < 200:  # Finger up
+                if lmList[8][2] < 200:
                     led.write(1)
                     cv2.putText(img, "LED ON", (10, 70),
                                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 3)
@@ -43,9 +37,10 @@ while True:
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     cv2.imshow("Hand LED Control", img)
-    if cv2.waitKey(1) & 0xFF == 27:  # Press ESC to exit
+    if cv2.waitKey(1) & 0xFF == 27:
         break
 
 cap.release()
 cv2.destroyAllWindows()
 board.exit()
+
